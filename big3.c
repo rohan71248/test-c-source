5bf1d717c0306f77ef2cf43ec6a427d9ff7019ab
rohan71248 <90970042+rohan71248@users.noreply.github.com>
2021-09-22 14:10:10 +0530

    Update big3.c

--- big3.c
+++ big3.c
@@ -15,3 +15,6 @@ void biggest3() {
     // if n3 is greater than both n1 and n2, n3 is the largest
     if (n3 >= n1 && n3 >= n2)
         printf("%.2f is the largest number.\n", n3);
+
+//    return 0;
+}
